45a33ed4b53814021fe059b4c96ef69eef9d6012
Malay Pandit <malay.pandit@dinereel.com>
2024-08-13 17:22:59 +0530

    added security configuration

--- src/main/kotlin/com/issuetracker/IssueTracker/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/issuetracker/IssueTracker/config/SecurityConfiguration.kt
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.DefaultSecurityFilterChain
-import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
@@ -18,43 +17,27 @@ class SecurityConfiguration (
     private val authenticationProvider: AuthenticationProvider,
     private val customAuthenticationEntryPoint: CustomAuthenticationEntryPoint
 ){
-
-
-
     @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-            .authorizeHttpRequests { authz ->
-                authz
-                    .requestMatchers("/**").permitAll() // Allow access to /users/** without authentication
-                    .anyRequest().authenticated()
+    fun securityFilterChain(
+        http:HttpSecurity,
+        jwtAuthenticationFilter: JwtAuthenticationFilter
+    ): DefaultSecurityFilterChain=
+        http.csrf{it.disable()}
+            .authorizeHttpRequests{
+                it.requestMatchers("/api/auth","/api/auth/refresh","/error",)
+                    .permitAll()
+                    .requestMatchers(HttpMethod.POST,"/api/user")
+                    .permitAll()
+                    .requestMatchers("/api/user**")
+                    .hasRole("ADMIN")
+                    .anyRequest()
+                    .fullyAuthenticated()
+
             }
-            .csrf().disable() // Disable CSRF for testing purposes
-        return http.build()
-    }
-
-
-//    @Bean
-//    fun securityFilterChain(
-//        http:HttpSecurity,
-//        jwtAuthenticationFilter: JwtAuthenticationFilter
-//    ): DefaultSecurityFilterChain=
-//        http.csrf{it.disable()}
-//            .authorizeHttpRequests{
-//                it.requestMatchers("/api/auth","/api/auth/refresh","/error",)
-//                    .permitAll()
-//                    .requestMatchers(HttpMethod.POST,"/api/user")
-//                    .permitAll()
-//                    .requestMatchers("/api/user**")
-//                    .hasRole("ADMIN")
-//                    .anyRequest()
-//                    .fullyAuthenticated()
-//
-//            }
-//            .sessionManagement {
-//            it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//            }
-//            .authenticationProvider(authenticationProvider)
-//            .addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter:: class.java)
-//            .build()
+            .sessionManagement {
+            it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter:: class.java)
+            .build()
 }
